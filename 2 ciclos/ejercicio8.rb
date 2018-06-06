e8e8ce6f11b27673b7250b01efd3ba800686d389
Jose Tomas <josetomascodecido@gmail.com>
2018-06-05 20:34:52 -0400

    exercise carpet2

--- 2 ciclos/ejercicio8.rb
+++ 2 ciclos/ejercicio8.rb
@@ -4,6 +4,10 @@
 a = ''
 10.times do |i|
 	
+	if (i+1).even?
+		a +="#{i+1}par "	
+	else 
+		a +="#{i+1}impar " 
 end
-
+end
 puts a
--- 2 ciclos/ejercicio9.rb
+++ 2 ciclos/ejercicio9.rb
@@ -11,3 +11,9 @@
 </table>
 
 =end
+a = "<table>\n	<tbody>\n		<tr>\n"
+3.times do |i|
+	a += "			<td> #{i+1} </td>\n"
+end
+a += "		</tr>\n    </tbody>\n</table>\n"
+puts a 
